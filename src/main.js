906e27039cf45ff8fb2ab73d2c4fba096aeece0c
gensahil1997 <me.sahilansari1@gmail.com>
2023-12-14 15:40:55 +0545

    new change

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 // src/main.js
+const { execSync } = require('child_process');
 const core = require('@actions/core');
 
 try {
-  const message = core.getInput('message');
-  console.log(`Changelog Action: ${message}`);
+  // Get the commit range
+  const commitRange = process.env.GITHUB_EVENT_NAME === 'push' ? process.env.GITHUB_SHA : 'HEAD^..HEAD';
+
+  // Generate changelog using conventional-changelog
+  const changelog = execSync(`npx conventional-changelog-cli -p angular -i CHANGELOG.md -s -r 0 --commit-path . --pkg ./package.json --release-count 0 --commit-path ./`)
+    .toString();
+
+  console.log('Changelog Action: Changelog updated successfully');
+  console.log(changelog);
+
 } catch (error) {
   core.setFailed(error.message);
 }
