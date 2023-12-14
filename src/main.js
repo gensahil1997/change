235ba847027828267c47c4f543d6688c56283243
gensahil1997 <me.sahilansari1@gmail.com>
2023-12-14 15:48:08 +0545

    new change

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
-// src/main.js
 const { execSync } = require('child_process');
+const { readFile } = require('fs').promises;
+const { relativeResolve } = require('your-module');
 const core = require('@actions/core');
 
-try {
-  // Get the commit range
-  const commitRange = process.env.GITHUB_EVENT_NAME === 'push' ? process.env.GITHUB_SHA : 'HEAD^..HEAD';
+async function generateChangelog() {
+  try {
+    // Get the commit range
+    const commitRange = process.env.GITHUB_EVENT_NAME === 'push' ? process.env.GITHUB_SHA : 'HEAD^..HEAD';
 
-  // Generate changelog using conventional-changelog
-  const changelog = execSync(`npx conventional-changelog-cli -p angular -i CHANGELOG.md -s -r 0 --commit-path . --pkg ./package.json --release-count 0 --commit-path ./`)
-    .toString();
+    // Read file content
+    const content = await readFile(relativeResolve(flags.context), 'utf8');
 
-  console.log('Changelog Action: Changelog updated successfully');
-  console.log(changelog);
+    // Generate changelog using conventional-changelog
+    const changelog = execSync(
+      `npx conventional-changelog-cli -p angular -i CHANGELOG.md -s -r 0 --commit-path . --pkg ./package.json --release-count 0 --commit-path ./`,
+      { input: content } // Pass file content as input
+    ).toString();
 
-} catch (error) {
-  core.setFailed(error.message);
+    console.log('Changelog Action: Changelog updated successfully');
+    console.log(changelog);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+// Call the asynchronous function
+generateChangelog();
